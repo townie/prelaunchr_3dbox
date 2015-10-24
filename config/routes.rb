0dd12ae2da5e0760ba806370924e5e9905dab54d
Keith Webber <keith@datto.com>
2015-10-23 23:02:14 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,14 @@ Prelaunchr::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
 
   root :to => "users#landing"
-
+  
   match 'users/create' => 'users#create'
 
   match 'refer-a-friend' => 'users#refer'
 
   match 'privacy-policy' => 'users#policy'
-
+  match 'rules' => 'users#rules'
+  
   unless Rails.application.config.consider_all_requests_local
       match '*not_found', to: 'users#redirect', :format => false
   end
